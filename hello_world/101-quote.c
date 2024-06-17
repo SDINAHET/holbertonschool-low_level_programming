b480614fedadb88e462ce315bbd87b8cb7257e37
SDINAHET <stephane.dinahet@gmail.com>
2024-06-17 23:41:36 +0000

    task9 projet9 v1

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -11,8 +11,8 @@ int main(void)
 	const char *msg_part1 = "and that piece of art is useful\" - ";
 	const char *msg_part2 = "Dora Korpar, 2015-10-19\n";
 	/* Print part 1 */
-	write(2, msg_part1, 35);  /* 2 is the file descriptor for standard error */
+	fwrite(2, msg_part1, 35);  /* 2 is the file descriptor for standard error */
 	/* Print part 2 */
-	write(2, msg_part2, 26);  /* 2 is the file descriptor for standard error */
+	fwrite(2, msg_part2, 26);  /* 2 is the file descriptor for standard error */
 	return (1);
 }
