7d808085d2589768a61a25f75cd71d0257ee34cc
SDINAHET <stephane.dinahet@gmail.com>
2024-06-17 14:11:12 +0000

    task4 projet9 v5

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
-
+#include <stdio.h>  /* Inclusion de la bibliotheque standard d'entree/sortie */
+/**
+ * main - Point d'entree du programme
+ * Cette fonction affiche un message a l'ecran en utilisant puts.
+ * Return: Toujours 0 (Succes)
+ */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");  /* Affiche une chaine de caracteres */
+return (0);  /* Retourne 0 pour indiquer que le programme s'est termine  avec succes */
 }
+
