a45b042dd2d3997129c9e22d1e87793428f27f69
SDINAHET <stephane.dinahet@gmail.com>
2024-06-17 14:44:05 +0000

    task5 projet9 v0

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>  /* Inclusion de la bibliotheque standard d'entree/sortie */
+/**
+ * main - Point d'entree du programme
+ * Cette fonction affiche un message a l'ecran en utilisant puts.
+ * Return: Toujours 0 (Succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
