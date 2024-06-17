4595f2090725fc51596e063bea7655b59999dfaa
SDINAHET <stephane.dinahet@gmail.com>
2024-06-17 22:58:51 +0000

    projet9 save

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the size of various types on the computer
+ * it is compiled and run on.
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
