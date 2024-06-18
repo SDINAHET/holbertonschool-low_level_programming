bc2184cedea9d87ea63c4455a8564805a0ff084f
SDINAHET <stephane.dinahet@gmail.com>
2024-06-18 08:23:26 +0000

    task 9 projet9 v6

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -3,8 +3,7 @@
 /**
  * main - Entry point
  * Description: Prints the size of various types on the computer
- * it is compiled and run on.
- * Return: 0 if successful
+ * Return: 1 if successful
  */
 int main(void)
 {
