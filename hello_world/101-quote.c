bf81f987e81b3028b995bbb799102cf7d8bf6156
SDINAHET <stephane.dinahet@gmail.com>
2024-06-18 08:57:31 +0000

    task9 projet9 v8

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,17 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Description: Prints the size of various types on the computer
- * Return: 1 if successful
- */
+* main - Prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+* followed by a new line, to standard error.
+*
+* Return: Always 1.
+*/
 int main(void)
 {
-	const char *msg_part1 = "and that piece of art is useful\" - ";
-	const char *msg_part2 = "Dora Korpar, 2015-10-19\n";
-	/* Print part 1 */
-	fwrite(2, msg_part1, 35);  /* 2 is the file descriptor for standard error */
-	/* Print part 2 */
-	fwrite(2, msg_part2, 26);  /* 2 is the file descriptor for standard error */
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
