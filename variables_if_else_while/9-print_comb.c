8f2205a4136e766cc9da853df0ec38b9e43532e0
SDINAHET <stephane.dinahet@gmail.com>
2024-06-19 22:44:24 +0000

    task9 projet10 v0

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Description all single digit numbers of base 10 starting from 0,
+* followed by a new line
+* Return: 0 succes
+* betty style doc for function main goes there
+*/
+int main(void)
 
+{
+int i;
+
+for (i = 0 ; i < 10; i++)
+{
+	putchar(i + '0 ,');
+}
+putchar('\$\n');
+
+return (0);
+}
