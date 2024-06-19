8bb9b728bd18d17356159a4dfcd30e4adec724a6
SDINAHET <stephane.dinahet@gmail.com>
2024-06-19 21:52:09 +0000

    task5 projet10 v3

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,16 +3,17 @@
 * main - Entry point of the program
 * Description all single digit numbers of base 10 starting from 0,
 * followed by a new line
-* Return:0 succes
+* Return: 0 succes
 * betty style doc for function main goes there
 */
 int main(void)
-{
-	int i;
 
-	for (int i = 0; i < 10; i++)
-	{
-		printf("%d\n", i);
-	}
-	return (0);
+{
+int i;
+
+for (i = 0 ; i < 10; i++)
+{
+	printf("%d\n", i);
+}
+return (0);
 }
