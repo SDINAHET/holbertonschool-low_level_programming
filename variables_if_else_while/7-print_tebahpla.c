cea77c354a673fb5923b853b8aeb34be9e607f15
SDINAHET <stephane.dinahet@gmail.com>
2024-06-19 22:27:43 +0000

    task7 projet10 v1

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+* main - Entry point of the program
+* Description all letter of alphabet de z vers a,
+* followed by a new line
+* Return: 0 succes
+* betty style doc for function main goes there
+*/
 int main(void)
 {
 char c;
