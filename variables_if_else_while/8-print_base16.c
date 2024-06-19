134af7677b2a807e913f4dd9e7ed3df13d7deac3
SDINAHET <stephane.dinahet@gmail.com>
2024-06-19 22:38:56 +0000

    task8 projet10 v1

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Description all prints all the numbers of base 16 in lowercase,
+* followed by a new line
+* Return: 0 succes
+* betty style doc for function main goes there
+*/
+int main(void)
+{
+char c;
 
+for (c = '0'; c <= '9'; c++)
+	{
+	putchar(c);
+	}
+
+for (c = 'a'; c <= 'f'; c++)
+	{
+	putchar(c);
+	}
+
+putchar('\n');
+
+return (0);
+}
