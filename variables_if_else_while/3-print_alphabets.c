3927d6c7b72e7af5c5f5f3582c0d295060d6debe
SDINAHET <stephane.dinahet@gmail.com>
2024-06-19 14:06:41 +0000

    task3 projet 10 v1

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Description print alphabet in lowercase
+* Return:0 succes
+* betty style doc for function main goes there
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	putchar(letter);
+	}
+for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
