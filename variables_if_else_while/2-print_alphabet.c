30d191856a503cbe16853a7414cbd2301b36adec
SDINAHET <stephane.dinahet@gmail.com>
2024-06-19 13:53:58 +0000

    task2 projet 10 v0

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Description print alphabet in lowercase
+* Return:0 succes
+* betty style doc for function main goes there
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
