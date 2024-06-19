07c0e25bc4e4cfd76829e1307c60fae728c38377
SDINAHET <stephane.dinahet@gmail.com>
2024-06-19 12:44:07 +0000

    task0 projet10 v10

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 /**
-* main tache0 projet10 Positive anything is better than negative nothing
+* main - Entry point of the program
+* Description n > 0 n==0 n<0
 * Return:0 succes
 * betty style doc for function main goes there
 */
@@ -13,10 +13,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	/* if the number is greater than 0: is positive n>0 */
-	/* if the number is 0: is zero n==0 */
-	/* if the number is less than 0: is negative n<0 */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
