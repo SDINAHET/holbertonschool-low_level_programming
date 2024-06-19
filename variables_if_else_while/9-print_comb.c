ef1f7018efab881d0687fe96039e604fdc61cc96
SDINAHET <stephane.dinahet@gmail.com>
2024-06-19 23:13:34 +0000

    task9 projet10 v2

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
 * main - Entry point of the program
-* Description all single digit numbers en letter of base 16 starting from 0,
-* followed by a new line
+* Description all single digit number with comma and space starting from 0,
+* followed by $ and a new line
 * Return: 0 succes
 * betty style doc for function main goes there
 */
@@ -14,13 +14,13 @@ char c;
 for (c = '0' ; c <= '9'; c++)
 {
 	putchar(c);
+	{
+	putchar(',');
+	putchar(' ');
+	}
 }
 
-for (c = 'a' ; c <= 'f'; c++)
-{
-	putchar(c);
-}
-putchar('\n');
+putchar('$' && '\n');
 
 return (0);
 }
