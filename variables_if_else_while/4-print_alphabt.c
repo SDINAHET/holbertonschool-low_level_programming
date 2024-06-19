76ca8f54f7f2293a1b069da6cf39c0859d3e9767
SDINAHET <stephane.dinahet@gmail.com>
2024-06-19 14:50:46 +0000

    task4 projet10 v0

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Description print alphabet in lowercase
+* Return:0 succes
+* betty style doc for function main goes there
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	if (letter != 'q' && letter != 'e')
+	{
+	putchar(letter);
+	}
+}
+	putchar('\n');
+	return (0);
+}
