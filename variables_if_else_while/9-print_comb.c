186eca38333dae561ed75233ce45ab75e5b53b8f
SDINAHET <stephane.dinahet@gmail.com>
2024-06-20 09:16:45 +0000

    task9 projet10 v10

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,14 +13,15 @@ int i;
 
 for (i = 0 ; i <= 9; i++)
 {
-	putchar(i + '0 ');
+	putchar(i + '0');
 	if (i < 9)
 	{
+	putchar(' '),
 	putchar(',');
 	}
 }
 
-putchar('$'), putchar('\n');
+putchar('\n');
 
 return (0);
 }
