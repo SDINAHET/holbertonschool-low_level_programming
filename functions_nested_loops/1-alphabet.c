9b78cbeebc807d27368be90cf298c70783ea3c95
SDINAHET <stephane.dinahet@gmail.com>
2024-06-21 14:04:32 +0000

    save file 2

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
-#include "1-main.h"
-#include <unistd.h>
+#include "main.h"
+
 /**
+* print_alphabet - Prints the alphabet in loxercase followed by a new line.
 * main - check the code
-* Description print alphabet with for
+* Description: print alphabet with for
 * print_alphabet - Prints the alphabet in lowercase followed by a new line.
 * Return: Always 0.
 */
 
 void print_alphabet(void)
 {
-	char c;
+	int c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
