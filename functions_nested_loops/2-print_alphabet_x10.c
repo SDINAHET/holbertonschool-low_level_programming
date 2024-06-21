a67cbde70feaf8ff5e8f81af8efa1448a4c12da4
SDINAHET <stephane.dinahet@gmail.com>
2024-06-21 22:25:00 +0000

    task2 projet 11 v0

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet - Prints the alphabet in loxercase followed by a new line.
+* main - check the code
+* Description: print alphabet  x 10 with for
+* print_alphabet - Prints the alphabet in lowercase followed by a new line.
+* Return: Always 0.
+*/
+
+void print_alphabet_x10(void)
+{
+	char c;
+	for (in == 0; i <= 10; i++)
+		{
+		for (c = 'a'; c <= 'z'; c++)
+		}
+		 _putchar(c);
+
+	}
+	}
+	_putchar('\n');
+}
