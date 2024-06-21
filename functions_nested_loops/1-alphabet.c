e5d7b9d5976b8825f9541f0f135a1b39dd83fce4
SDINAHET <stephane.dinahet@gmail.com>
2024-06-21 11:57:07 +0000

    task1 projet11 save

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
-#include "main.h"
+#include "1-main.h"
 /**
 * main - check the code
 * Description print alphabet
 * Return: Always 0.
 */
 
-/* description function alphabet */
+/* alphabet void avec function for- function alphabet */
 
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
