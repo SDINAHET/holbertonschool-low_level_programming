6027703eca141f79c9dd739a9876f5942f9bc5ed
SDINAHET <stephane.dinahet@gmail.com>
2024-06-23 16:38:51 +0000

    task12 100 projet11 v2

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -6,7 +6,8 @@
 */
 void print_times_table(int n)
 {
-	int i, j product;
+	int i, j, product;
+
 	if (n < 0 || n > 15)
 		return;
 
@@ -19,22 +20,25 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-			else if (product > 100)
-				_putchar(' ');
+				if (product < 10)
+					_putchar(' '), _putchar (' ');
+				else if (product > 100)
+					_putchar(' ');
+			}
+			if (product < 10)
+				_putchar(product + '0');
+			else if (product < 100)
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+			else
+			{
+				_putchar(product / 100 + '0');
+				_putchar((product / 10) % 10 + '0');
+				_putchar(product % 10 + '0');
+			}
 		}
-		if (product < 10)
-			_putchar(product + '0');
-		else if (product < 100)
-		{
-			_putchar(product / 10 + '0');
-			_putchar(product % 10 + '0');
-		}
-		else
-		{
-			_putchar(product / 100 + '0');
-			_putchar((product / 10) % 10 + '0');
-			_putchar(product % 10 + '0');
-		}
-	}
 	_putchar('\n');
+	}
 }
