1463a2c1e682971dda37895bccc92069dad40e1d
SDINAHET <stephane.dinahet@gmail.com>
2024-06-24 13:46:41 +0000

    task5 projet12 v1

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,29 +1,35 @@
 #include "main.h"
 
 /**
-* more_numbers - Prints numbers in 1 line, starting with 0
-* 01234567891011121314 de 0 a 14
+* more_numbers - Prints numbers from 0 to 14, ten times in a single line.
+* Each set of numbers is followed by a newline.
 * _putchar can use three times in your code
+* SD
 */
 void more_numbers(void)
 {
 	int i;
+	int j;
 	int first_digit;
 	int second_digit;
 
-	for (i = 0; i <= 14; i++)
+	for (j = 0 ; j <= 10; j++)
 	{
-		if (i > 9) /*nombre au dessus de 10 entre 10 et 14*/
+		for (i = 0; i <= 14; i++)
 		{
-			first_digit = i / 10; /*appel 1er chiffre*/
-			second_digit = i % 10; /*appel 2eme chiffre*/
-			_putchar (first_digit + '0'); /*print first digit*/
-			_putchar (second_digit + '0'); /*print secon digit*/
+			if (i > 9) /*nombre au dessus de 10 entre 10 et 14*/
+			{
+				first_digit = i / 10; /*appel 1er chiffre*/
+				second_digit = i % 10; /*appel 2eme chiffre*/
+				_putchar (first_digit + '0'); /*print first digit*/
+				_putchar (second_digit + '0'); /*print secon digit*/
+			}
+			else
+			{
+				_putchar (i + '0');
+			}
 		}
-		else
-		{
-			_putchar (i + '0'); /*print first digit de 0 a 9*/
-		}
+		_putchar ('\n');
 	}
 	_putchar ('\n'); /*print newline after code*/
 }
