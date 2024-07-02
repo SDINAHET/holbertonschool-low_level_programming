02f47dbce2bfbe712753152bd3a1939d91774bd5
SDINAHET <stephane.dinahet@gmail.com>
2024-07-02 09:22:36 +0000

    task5 strstr projet15 v0

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of the substring needle in the string haystack
+ * @haystack: the string to search in
+ * @needle: the substring to search for
+ *
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *haystack_ptr, *needle_ptr;
+
+	while (*haystack)
+	{
+		haystack_ptr = haystack;
+		needle_ptr = needle;
+
+		while (*haystack_ptr == *needle_ptr && *needle_ptr)
+		{
+			haystack_ptr++;
+			needle_ptr++;
+		}
+
+		if (*needle_ptr == '\0')
+		{
+			return haystack;
+		}
+
+		haystack++;
+	}
+
+	return NULL;
+}
