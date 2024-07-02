2d3cacd50c724bec0cb8c308aa1b9be1c2adb623
SDINAHET <stephane.dinahet@gmail.com>
2024-07-02 21:30:56 +0000

    task1 projet16 v3

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,11 +6,30 @@
 */
 void _print_rev_recursion(char *s)
 {
-        if (*s > '\0')  /* If the current character is not null */
+	if (*s != '\0')  /* If the current character is not null */
 /* if (*s) */
-/* if (*s > '0') */
-        {
-                _print_rev_recursion(s + 1);  /* Recursively call with the next character */
-                _putchar(*s);  /* Print the current character */
+/* if (*s > '\0') */
+/* if (*s != '\0' */
+	{
+		 _print_rev_recursion(s + 1);  /* Recursively call with the next character */
+		_putchar(*s);  /* Print the current character */
+	}
+}
+#include "main.h"
+
+/**
+* _strlen_recursion - Returns the length of a string
+* @s: The string to be printed in reverse
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')  /* If the current character is not null */
+/* if (*s) */
+/* if (*s > '\0') */
+/* if (*s != '\0' */
+	{
+		_strlen_recursion(s + 1);  /* Recursively call with the next character */
+		 _putchar(*s);  /* Print the current character */
         }
+        return (s*);
 }
