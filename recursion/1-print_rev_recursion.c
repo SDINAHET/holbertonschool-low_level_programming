5d315da8aff4d135f695408a577d6176a74956b3
SDINAHET <stephane.dinahet@gmail.com>
2024-07-02 20:44:07 +0000

    task1 projet16 v1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -6,7 +6,9 @@
 */
 void _print_rev_recursion(char *s)
 {
-	if (*s)  /* If the current character is not null */
+	if (*s > '\0')  /* If the current character is not null */
+/* if (*s) */
+/* if (*s > '0') */
 	{
 		_print_rev_recursion(s + 1);  /* Recursively call with the next character */
 		_putchar(*s);  /* Print the current character */
