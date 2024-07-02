b9353b433982465d4585eb1c8f948f90b0ad15aa
SDINAHET <stephane.dinahet@gmail.com>
2024-07-02 08:32:25 +0000

    task3 strspn projet15 v2

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,20 +14,32 @@ unsigned int _strspn(char *s, char *accept)
 	char *a;
 
 	while (*s)
+/*This loop continues as long as the current character pointed to by s is not the null terminator ('\0').*/
 	{
-		for (a = accept; *a; a++)
+		for (a = accept; *a; a++) /*This loop iterates through each character in the accept string.*/
 		{
 			if (*s == *a)
 			{
 				count++;
 				break;
+/*This checks if the current character in s matches the current character in accept*/
+/*If a match is found, it increments the count and breaks out of*/
+/* the for loop to continue checking the next character in s.*/
 			}
 		}
 		if (*a == '\0')
 		{
 			break;
+/*After the for loop, this checks if *a is the null terminator ('\0').*/
+/*If *a is '\0', it means that no match was found in accept for the current character in s.*/
+/*In this case, the while loop breaks because the initial segment/*
+/*of s that consists only of characters from accept has ended.*/
 		}
 		s++;
+/*Moves the pointer s to the next character in the string to check.*/
 	}
 	return (count);
+/*After the loops have completed, the function returns the value of count,*/
+/* which is the number of bytes in the initial segment*/
+/* of s that consist only of bytes from accept.*/
 }
