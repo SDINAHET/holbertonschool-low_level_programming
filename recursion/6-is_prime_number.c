c545d672920186382e148850ecc10cfe525a63ca
SDINAHET <stephane.dinahet@gmail.com>
2024-07-03 08:31:50 +0000

    task6 projet16 v3

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -20,3 +20,15 @@ int is_prime_number(int n)
 	}
 	return (check_prime(n, 2));
 }
+
+/*check_prime est une fonction récursive interne à is_prime_number*/
+/* qui vérifie si un nombre n est divisible par un nombre i.*/
+
+/*Si n est inférieur ou égal à 1, elle renvoie 0 (car les*/
+/* nombres inférieurs ou égaux à 1 ne sont pas premiers).*/
+/*Si i * i est supérieur à n, elle renvoie 1 (car n n'est pas divisible par*/
+/*un nombre inférieur à sa racine carrée).*/
+/*Si n est divisible par i, elle renvoie 0 (car n n'est pas premier).*/
+/*Sinon, elle s'appelle elle-même avec l'entier suivant (i + 1).*/
+/*is_prime_number est la fonction principale qui appelle*/
+/* check_prime en commençant par 2.*/
