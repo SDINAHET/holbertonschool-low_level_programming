41293b09954a720b7c1812c404d9c414ec766eba
SDINAHET <stephane.dinahet@gmail.com>
2024-07-03 08:06:19 +0000

    task6 projet16 v0

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* is_prime_helper - helps to determine if a number is prime
+* @n: the number to check
+* @i: the current divisor being tested
+*
+* Return: 1 if n is prime, 0 otherwise
+*/
+int is_prime_helper(int n, int i)
+{
+
+/**
+* is_prime_number - checks if a number is a prime number
+* @n: the number to check
+*
+* Return: 1 if n is prime, 0 otherwise
+*/
+int is_prime_number(int n)
