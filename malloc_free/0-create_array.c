51594f81961db3f9415320bbdbf2b6a7379a15de
SDINAHET <stephane.dinahet@gmail.com>
2024-07-08 20:57:49 +0000

    task0 projet18 v3

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	array = (char *)malloc(size * sizeof(char));
+	array = malloc(size * sizeof(char)); /*array = (char *)malloc(size * sizeof(char));*/
 	if (array == NULL)
 		return (NULL);
 
