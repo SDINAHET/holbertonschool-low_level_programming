b8d79c359b306a6ffca99b234fb90f515996b0e9
SDINAHET <stephane.dinahet@gmail.com>
2024-07-09 14:40:03 +0000

    task4 projet18 v1

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -9,14 +9,23 @@
 */
 void free_grid(int **grid, int height)
 {
-	int i;
+	int i; /*1_declaration de la variable*/
 
-	if (grid == NULL || height <= 0)
+	if (grid == NULL || height <= 0) /*2_validation des parametres*/
 	return;
+/*Si grid est NULL ou si height est <= 0, la fonction retourne immediatement.*/
+/* Cela empeche les erreurs de segmentation et assure que les parametres */
+/*sont valides avant de proceder a la liberation de la memoire*/
 
-	for (i = 0; i < height; i++)
+	for (i = 0; i < height; i++) /*3_liberation de chaque ligne de la grille*/
 	{
 		free(grid[i]);
 	}
-	free(grid);
+/*La boucle for parcourt chaque ligne de la grille.*/
+/* grid[i] est un pointeur vers une ligne (un tableau d'entiers)*/
+/* et free(grid[i]) libère la mémoire allouée pour cette ligne.*/
+	free(grid); /*4_liberation du tableau du pointeurs*/
+/*Apres avoir libere�toutes les lignes individuelles, la memoire alloue*/
+/* pour le tableau de pointeurs lui-même (grid) est libere.*/
+/* Cela complete la liberation de la grille bidimensionnelle entiere.*/
 }
