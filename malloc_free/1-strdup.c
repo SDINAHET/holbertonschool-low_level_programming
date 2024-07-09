ed1f1b3a9251ad621319ee3bd0ba0078ef6bf21c
SDINAHET <stephane.dinahet@gmail.com>
2024-07-09 08:59:15 +0000

    task1 projet18 v7

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL); /*Returns NULL if str = NULL*/
 
-	for (length = 0; str[length] != '\0'; length++);
+	for (length = 0; str[length] != '\0'; length++)
 
 	dup = malloc((length + 1) * sizeof(char));
 	if (dup == NULL)
