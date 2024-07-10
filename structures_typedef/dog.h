2ec3b3222c7569c1ebbae7dc591069fcb94eeda2
SDINAHET <stephane.dinahet@gmail.com>
2024-07-10 22:41:20 +0000

    task0 projet20 v0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* struct dog - a new type describing a dog
+* @name: name of the dog (char pointer)
+* @age: age of the dog (float)
+* @owner: owner of the dog (char pointer)
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
