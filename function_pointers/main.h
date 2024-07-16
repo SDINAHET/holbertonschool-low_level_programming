5604da307a71c427427a0f835b69c52f898467b6
SDINAHET <stephane.dinahet@gmail.com>
2024-07-16 09:05:09 +0000

    save file v1

---
+++ function_pointers/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stlib.h>
+
+int _putchar(char c);
+/*0-*/
+void print_name(char *name, void (*f)(char *));
+/*1-*/
+void array_iterator(int *array, size_t size, void (*action)(int));
+/*2-*/
+int int_index(int *array, int size, int (*cmp)(int));
+/*3-*/
