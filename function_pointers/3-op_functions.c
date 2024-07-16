d08eec471f6f522bcda666f8fbc499aa859f9eca
SDINAHET <stephane.dinahet@gmail.com>
2024-07-16 17:28:34 +0000

    task3 projet21 3-op_functions.c v0

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+* op_add - check addition a+b
+*
+* Return: a+b
+*/
+int op_add(int a, int b);
+{
+	return (a + b);
+}
+
+/**
+* op_sub - check soustraction a-b
+*
+* Return: a-b
+*/
+int op_sub(int a, int b);
+{
+	return (a - b);
+}
+
+/**
+* op_mul - check multiplication a*b
+*
+* Return: a*b
+*/
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+
+/**
+* op_div - check division a/b
+*
+* Return: a/b
+*/
+int op_div(int a, int b);
+{
+	return (a / b);
+}
+
+/**
+* op_mod - check modulo_% a%b
+*
+* Return: a%b
+*/
+int op_mod(int a, int b);
+{
+	return (a % b);
+}
