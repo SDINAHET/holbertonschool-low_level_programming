ecfb6ec7f821cfc6c4248c75df31ae40a2a2f2c0
SDINAHET <stephane.dinahet@gmail.com>
2024-07-16 17:13:07 +0000

    save 3-calc.h v1

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -17,11 +17,24 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+int _putchar(char c)
+
+/*addition*/
 int op_add(int a, int b);
+
+/*soustraction*/
 int op_sub(int a, int b);
+
+/*multiplication*/
 int op_mul(int a, int b);
+
+/*division*/
 int op_div(int a, int b);
+
+/*modulo*/
 int op_mod(int a, int b);
+
+/*3-get_op_func.c*/
 int (*get_op_func(char *s))(int, int)
 
 #endif /* CALC_H */
