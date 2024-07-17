d2cd610544fc6b352c082b0790508b80849e4fc3
SDINAHET <stephane.dinahet@gmail.com>
2024-07-17 14:39:38 +0000

    task0 projet22 v2

--- variadic_functions/0-main.c
+++ variadic_functions/0-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int sum;
 
-	sum = sum_them_all(2, 98, 1024);
-	printf("%d\n", sum);
-	sum = sum_them_all(4, 98, 1024, 402, -1024);
-	printf("%d\n", sum);
+	sum = sum_them_all(2, 98, 1024); /*test avec 2 arguments*/
+	printf("%d\n", sum); /*affiche 1122 98+1024*/
+	sum = sum_them_all(4, 98, 1024, 402, -1024); /*test avec 4 arguments*/
+	printf("%d\n", sum); /*affiche 500 98+1024+402-1024*/
 	return (0);
 }
