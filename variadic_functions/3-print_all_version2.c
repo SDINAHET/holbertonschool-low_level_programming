77dbcce4e4f15f76ac96ec3e2e05a2c983ea2f02
SDINAHET <stephane.dinahet@gmail.com>
2024-07-17 23:52:54 +0000

    task3 projet22 v3

--- variadic_functions/3-print_all_version2.c
+++ variadic_functions/3-print_all_version2.c
@@ -20,38 +20,16 @@ void print_all(const char * const format, ...)
 
 	va_start(args, format);
 
-	while (format && format[i])
+	while i
 	{
-		j = 0;
-		while (separator[j] != '\0')
-			j++;
-
-		switch (format[i])
+		while j
 		{
-			case 'c':
-				 printf("%s%c", separator, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", separator, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", separator, va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(nil)";
-				printf("%s%s", separator, str);
-				break;
-			default:
-				i++;
-				continue;
+			str = va_arg(args, char *);
 		}
-		separator = ", ";
-		i++;
 	}
 
 	va_end(args);
 
 	printf("\n");
 }
+2
