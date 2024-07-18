b66d7039469423c40070f8aab7356d6f2ecca589
SDINAHET <stephane.dinahet@gmail.com>
2024-07-18 00:05:15 +0000

    task3 projet22 v5

--- variadic_functions/3-print_all(chatgptok).c
+++ variadic_functions/3-print_all(chatgptok).c
@@ -55,3 +55,4 @@ void print_all(const char * const format, ...)
 
 	printf("\n");
 }
+2
---
+++ variadic_functions/_3-print_all.c
@@ -0,0 +1,36 @@
+#include <stdarg.h> /*obligatoire avec va_list va_start va_arg va_end*/
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* print_all - Prints anything based on the format provided.
+* @format: A list of types of arguments passed to the function.
+*
+* Return: void.
+*/
+void print_all(const char * const format, ...)
+/*const char *separator est la chaine a imprimer entre les nombres.*/
+/*const format est le format c/i/f/s pour la fonction*/
+/*... indique qu'il s'agit d'une fonction variadique,*/
+/* acceptant un nombre variable d'arguments.*/
+{
+	va_list args;
+	unsigned int i = 0, j = 0;
+	char *str, *separator = "";
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (separator[j] != '\0')
+			j++;
+
+		separator = ", ";
+		i++;
+	}
+
+	va_end(args);
+
+	printf("\n");
+}
