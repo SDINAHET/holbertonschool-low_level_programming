78b8c5f3fb584148ec0d220aea6caca26da474d2
SDINAHET <stephane.dinahet@gmail.com>
2024-07-19 15:01:57 +0000

    task4 100 projet22 v12

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -9,14 +9,14 @@ section .text
 	global _start
 
 _start:
-	; Write "Hello, World" to stdout
+	; sys_write (system call number 1)
 	mov rax, 1          ; syscall number for sys_write
 	mov rdi, 1          ; file descriptor 1 is stdout
 	mov rsi, hello      ; pointer to the hello message
 	mov rdx, hello_len  ; length of the message
 	syscall             ; invoke operating system to do the write
 
-	; Exit the program
+	; sys_exit the program
 	mov rax, 60         ; syscall number for sys_exit
 	xor rdi, rdi        ; exit code 0
 	syscall             ; invoke operating system to exit
