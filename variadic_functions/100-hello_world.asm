d53e7ee64c8f8661b99973928ecb25b9f1da31b8
SDINAHET <stephane.dinahet@gmail.com>
2024-07-19 12:07:51 +0000

    task100 4 projet22 v1

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,19 @@
+section .data
+	hello db 'Hello, World', 0x0A  ; The string to print with a newline character at the end
+	hello_len equ $ - hello        ; The length of the string
+
+section .text
+	global _start
+
+_start:
+	; Write "Hello, World" to stdout
+	mov rax, 1          ; syscall number for sys_write
+	mov rdi, 1          ; file descriptor 1 is stdout
+	mov rsi, hello      ; pointer to the hello message
+	mov rdx, hello_len  ; length of the message
+	syscall             ; invoke operating system to do the write
+
+	; Exit the program
+	mov rax, 60         ; syscall number for sys_exit
+	xor rdi, rdi        ; exit code 0
+	syscall             ; invoke operating system to exit
--- variadic_functions/100-hello_world.c
+++ variadic_functions/100-hello_world.c
@@ -1,18 +1,1 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main()
-{
-	const char *message = "Hello, World\n";
-
-	write(1, message, 13);
-	return (0);
-}
+#include <unistd.h>/*** main - Entry point** Return: Always 0 (Success)*/int main(void){	const char *message = "Hello, World\n";	write(1, message, 13);	return (0);}
