f983399e027cd03a4dbbc28d5279d8f93a942294
SDINAHET <stephane.dinahet@gmail.com>
2024-07-23 00:48:09 +0000

    task3 projet22 v30

--- variadic_functions/3-print_all_v6.c
+++ variadic_functions/3-print_all_v6.c
@@ -2,54 +2,24 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
-/**
- * print_char - Print a character from a va_list.
- * @args: A va_list containing the character to be printed.
- *
- * Description: This function extracts a character from the va_list
- * and prints it to the standard output.
- */
 void print_char(va_list args)
 {
 	printf("%c", va_arg(args, int));
 }
 
-/**
- * print_int - Print an integer from a va_list.
- * @args: A va_list containing the integer to be printed.
- *
- * Description: This function extracts an integer from the va_list
- * and prints it to the standard output.
- */
 void print_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
 
-/**
- * print_float - Print a floating-point number from a va_list.
- * @args: A va_list containing the floating-point number to be printed.
- *
- * Description: This function extracts a floating-point number from
- * the va_list and prints it to the standard output.
- */
 void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
 
-/**
- * print_string - Print a string from a va_list.
- * @args: A va_list containing the string to be printed.
- *
- * Description: This function extracts a string from the va_list and
- * prints it to the standard output. If the string is NULL, it prints
- * "(null)" instead.
- */
 void print_string(va_list args)
 {
 	char *str = va_arg(args, char *);
-
 	if (str == NULL)
 	{
 		str = "(nil)";
@@ -101,3 +71,4 @@ void print_all(const char * const format, ...)
 	printf("\n");
 	va_end(args);
 }
+2
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -13,11 +13,17 @@ void print_strings(const char *separator, const unsigned int n, ...);
 /*3*/
 void print_all(const char * const format, ...);
 
+
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+
 /*3-definir une fonction en fonction du format*/
 typedef struct printall
 {
 	char cara;
-	void (*func)(va_list arg);
+	void (*func)(va_list args);
 } printall_t;
 
 #endif /* VARIADIC_FUNCTIONS_H */
