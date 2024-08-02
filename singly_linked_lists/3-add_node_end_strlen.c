50d13cad8f549b3c42521376dfb2ddf7b93a4451
SDINAHET <stephane.dinahet@gmail.com>
2024-08-02 13:23:15 +0000

    fix strlen forbiden task3 project24

--- singly_linked_lists/3-add_node_end_strlen.c
+++ singly_linked_lists/3-add_node_end_strlen.c
@@ -17,42 +17,41 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;  /* Pointer to the new node to be added */
 	list_t *temp;      /* Temporary pointer to traverse the list */
-	char *str_dup;	/*Pointer for the duplicate string*/
-	size_t len;	/*lenght of the string*/
-
-	if (str == NULL) /* Check if str is NULL */
-		return (NULL);
 
 	/* Create and initialize the new node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
+	{
 		return (NULL);  /* Return NULL if memory allocation fails */
+	}
 
-	str_dup = strdup(str); /* Duplicate the string */
-	if (str_dup == NULL)
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
 		free(new_node);  /* Free the node if strdup fails */
 		return (NULL);
 	}
-	len = strlen(str); /* Get the length of the string */
 
-	/* Assign duplicated string and its length to the new node */
-	new_node->str = str_dup;
-	new_node->len = len;
+	new_node->len = strlen(str);
 	new_node->next = NULL;
 
 	/* If the list is empty, make the new node the head */
 	if (*head == NULL)
+	{
 		*head = new_node;
+	}
 	else
-	{  /* Traverse to the end of the list */
+	{
+		/* Traverse to the end of the list */
 		temp = *head;
 		while (temp->next != NULL)
 		{
 			temp = temp->next;
 		}
-		temp->next = new_node;  /* Link the new node at the end of the list */
+		/* Link the new node at the end of the list */
+		temp->next = new_node;
 	}
 
 	return (new_node);  /* Return the address of the new node */
 }
+
