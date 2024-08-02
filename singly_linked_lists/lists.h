0661a1028da147a0103cefc77eea7bbce8ec272f
SDINAHET <stephane.dinahet@gmail.com>
2024-08-02 12:09:30 +0000

    fix betty and space lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -12,28 +12,21 @@
  * @len: length of the string
  * @next: points to the next node
  *
- * Lorsqu'une chaîne est allouée avec malloc, vous obtenez
- * un pointeur vers le début de cette mémoire allouée,
- * que vous pouvez utiliser comme une chaîne de caractères.
- *
  * Description: singly linked list node structure
  * This structure represents a node in a singly linked list.
  * The `str` field is a pointer to a string that has been allocated using
  * malloc. This means the memory for this string needs to be freed when
  * the node is no longer needed to avoid memory leaks.
  */
-typedef struct list_s /*important cette structure des singly linky lists*/
+typedef struct list_s
 {
-	char *str;              /* Pointeur vers une chaîne de caractères */
-	unsigned int len;      /* Longueur de la chaîne de caractères */
-	struct list_s *next;   /* Pointeur vers le prochain nœud de la liste */
+	char *str;              /* Pointer to a string */
+	unsigned int len;      /* Length of the string */
+	struct list_s *next;   /* Pointer to the next node in the list */
 } list_t;
 
 /* Function prototypes */
 int _putchar(char c);
-
-/*0*/
-size_t print_list(const list_t *h);
 
 /*1*/
 size_t list_len(const list_t *h);
