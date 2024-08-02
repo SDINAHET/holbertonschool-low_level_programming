b855c388791749c510f5113bc668334b13c930e5
SDINAHET <stephane.dinahet@gmail.com>
2024-08-02 12:51:17 +0000

    fix strlen forbidden for task2 projet24

--- singly_linked_lists/2-add_node_strlen.c
+++ singly_linked_lists/2-add_node_strlen.c
@@ -7,28 +7,26 @@
  * @head: A pointer to a pointer to the head of the list_t list.
  * @str: The string to be added to the new node.
  *
- * Return: The address of the new element, or NULL if it failed.
- *
  * Description:
  * This function allocates memory for a new node, duplicates the string d,
  * and inserts the new node at the beginning of the list. If memory allocation
  * for the node or string duplication fails, the function returns NULL.
  * The new node's `next` pointer is set to the current head of the list, and
  * the head is updated to point to the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 
 {
 	list_t *new_node;
 	char *str_dup;
-	size_t len;
 
-	/* Allocate memory for the new node */
+	/*Allocate memory for the new node*/
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-
-	/* Duplicate the string */
+	/*Duplicate the string and assign it to the new node*/
 	str_dup = strdup(str);
 	if (str_dup == NULL)
 	{
@@ -36,18 +34,13 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	/* Calculate the length of the duplicated string */
-	len = 0;
-	while (str_dup[len] != '\0')
-	len++;
-
-	/* Assign the duplicated string and its length to the new node */
 	new_node->str = str_dup;
-	new_node->len = len;
+	new_node->len = strlen(str);
 
-	/* Insert the new node at the beginning of the list */
+	/*Insert the new node at the beginning of the list*/
 	new_node->next = *head;
 	*head = new_node;
 
 	return (new_node);
 }
+2
