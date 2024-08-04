c6dbd8ae38a2d34648630671d76c8ecd5d3afa63
SDINAHET <stephane.dinahet@gmail.com>
2024-08-04 21:10:48 +0200

    task3 projet27 v7

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,7 +5,11 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/stat.h>
-/*Inclure cette bibliothèque pour les macros de permissions*/
+
+/* Function prototypes */
+void print_error(const char *message, int exit_code);
+void copy_file(int file_from, int file_to);
+
 /**
  * print_error - Prints error messages to standard error.
  * @message: Error message to print.
@@ -18,6 +22,26 @@ void print_error(const char *message, int exit_code)
 }
 
 /**
+ * copy_file - Copies content from one file descriptor to another.
+ * @file_from: Source file descriptor.
+ * @file_to: Destination file descriptor.
+ */
+void copy_file(int file_from, int file_to)
+{
+	char buffer[1024];
+	ssize_t r, w;
+
+	while ((r = read(file_from, buffer, sizeof(buffer))) > 0)
+	{
+		w = write(file_to, buffer, r);
+		if (w != r)
+			print_error("Error: Can't write to file\n", 99);
+	}
+	if (r == -1)
+		print_error("Error: Can't read from file\n", 98);
+}
+
+/**
  * main - Copies the content of one file to another.
  * @argc: Number of command-line arguments.
  * @argv: Array of command-line arguments.
@@ -26,8 +50,7 @@ void print_error(const char *message, int exit_code)
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, r, w;
-	char buffer[1024];
+	int file_from, file_to;
 	mode_t file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
@@ -35,44 +58,19 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		print_error("Error: Can't read from file ", 98);
+
 	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, file_perm);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(file_from);
-		exit(99);
+		print_error("Error: Can't write to file\n", 99);
 	}
-	while ((r = read(file_from, buffer, sizeof(buffer))) > 0)
-	{
-		w = write(file_to, buffer, r);
-		if (w != r)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(file_from);
-			close(file_to);
-			exit(99);
-		}
-	}
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(file_from);
-		close(file_to);
-		exit(98);
-	}
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+
+	copy_file(file_from, file_to);
+
+	if (close(file_from) == -1 || close(file_to) == -1)
+		print_error("Error: Can't close fd\n", 100);
+
 	return (0);
 }
